6f7127fb5a681ade03f34a567c6297db6b3edb43
bzy-debug <bzy_sustech@foxmail.com>
2025-03-02 00:00:28 +0800

    change name add add last used

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,42 @@
 import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
+  let lastUsed: string | undefined;
   context.subscriptions.push(
-    vscode.commands.registerCommand("run-command.runCommand", async () => {
-      const commandId = await vscode.window.showQuickPick(
-        vscode.commands.getCommands(true),
-        { placeHolder: "Select a command to run" },
-      );
-      if (commandId === undefined) {
-        return;
-      }
-      try {
-        await vscode.commands.executeCommand(commandId);
-      } catch (error) {
-        if (error instanceof Error) {
-          vscode.window.showErrorMessage(
-            `Failed to execute command: ${error.message}`,
-          );
-        } else {
-          vscode.window.showErrorMessage("Failed to execute command");
+    vscode.commands.registerCommand(
+      "run-command-by-id.runCommand",
+      async () => {
+        const commands = await vscode.commands.getCommands(true);
+        const items: vscode.QuickPickItem[] = [];
+        if (lastUsed !== undefined) {
+          items.push({ label: lastUsed, description: "Last used" });
         }
-      }
-    }),
+        for (const command of commands) {
+          if (command === lastUsed) {
+            continue;
+          }
+          items.push({ label: command });
+        }
+        const item = await vscode.window.showQuickPick(items, {
+          placeHolder: "Select a command to run",
+        });
+        if (item === undefined) {
+          return;
+        }
+        lastUsed = item.label;
+        try {
+          await vscode.commands.executeCommand(item.label);
+        } catch (error) {
+          if (error instanceof Error) {
+            vscode.window.showErrorMessage(
+              `Failed to execute command: ${error.message}`,
+            );
+          } else {
+            vscode.window.showErrorMessage("Failed to execute command");
+          }
+        }
+      },
+    ),
   );
 }
 
